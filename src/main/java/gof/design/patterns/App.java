eb97a18d0d591f50bb644a39f87d92b9e5ee3f4d
v.muravskyi <v.muravskyi@gmail.com>
2024-07-27 18:49:58 +0200

    initial

---
+++ src/main/java/gof/design/patterns/App.java
@@ -0,0 +1,13 @@
+package gof.design.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
